7bec049287b725f1e7ee388ebf2d4cdfcc768a06
Diego-Luna <diegofcolunalopez@gmail.com>
2022-05-04 14:41:58 -0500

    Mostrar una alerta

--- fl_components_app/lib/screens/alert_screen.dart
+++ fl_components_app/lib/screens/alert_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+
+  void  displayDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder:  (contex) {
+        return AlertDialog(
+          elevation: 10,
+          title: const Text('titulo'),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text("Este es el contenidod e la alerta"),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100)
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(contex),
+              child: const Text("Cancel")
+            )
+          ],
+        );
+      }
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +44,7 @@ class AlertScreen extends StatelessWidget {
             style: TextStyle(fontSize: 16,),
             )
           ),
-          onPressed: () {},
+          onPressed: () => displayDialog(context),
         )
       ),
       floatingActionButton: FloatingActionButton(
