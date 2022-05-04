9cdd6c4f39bd9f82ae0d5a4b075b90ef24f3575f
Diego-Luna <diegofcolunalopez@gmail.com>
2022-05-04 11:38:22 -0500

    Ajustar el tamaño de la imagen

--- fl_components_app/lib/widgets/custom_card_type_2.dart
+++ fl_components_app/lib/widgets/custom_card_type_2.dart
@@ -1,20 +1,35 @@
+import 'package:fl_components_app/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget{
-  
+
   const CustomCardType2({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 10,
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      elevation: 20,
+      shadowColor: AppTheme.prymary.withOpacity(0.2),
       child: Column(
-        children: const[
+        children: [
           // Image(
-          FadeInImage(
+          const FadeInImage(
             image: NetworkImage("http://cdn.eso.org/images/screen/millour-01-cc.jpg"),
             placeholder: AssetImage('assets/gifs/jar-loading.gif'),
-          )
+            width: double.infinity,
+            height: 260,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(right: 20, left: 10, bottom:  10),
+            child: const Text("Un hermoso paisaje")
+          ),
         ],
       ),
     );
