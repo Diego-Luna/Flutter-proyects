4243f245ba032e091bb7b4ecd682369898b4320e
Diego-Luna <diegofcolunalopez@gmail.com>
2022-05-10 10:54:18 -0500

    Widget Form

--- fl_components_app/lib/screens/inputs_screen.dart
+++ fl_components_app/lib/screens/inputs_screen.dart
@@ -6,6 +6,17 @@ class InputsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+    final Map<String, String> formValues = {
+      'first_name'  : 'diego',
+      'last_name'   : ':luna',
+      'email'        : 'diego@gmail.com',
+      'password': '123456',
+      'role': 'Admin'
+    };
+
     return  Scaffold(
       appBar: AppBar(
         title: const Text("Inputs y Forms"),
@@ -13,17 +24,37 @@ class InputsScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: const [
-              CustomIputField( labelText: "nombre", hintText: "Nombre del usuario"),
-              SizedBox(height: 30),
-              CustomIputField( labelText: "Apellido", hintText: "Apellido del usuario"),
-              SizedBox(height: 30),
-              CustomIputField( labelText: "Correo", hintText: "Correo del usuario", keyboardType: TextInputType.emailAddress,),
-              SizedBox(height: 30),
-              CustomIputField( labelText: "Contrasena", hintText: "Contrasena del usuario", obscureText: true),
-              SizedBox(height: 30),
-            ],
+          child: Form(
+            key: myFormKey,
+            child: Column(
+              children: [
+                const CustomIputField( labelText: "nombre", hintText: "Nombre del usuario"),
+                const SizedBox(height: 30),
+                const CustomIputField( labelText: "Apellido", hintText: "Apellido del usuario"),
+                const SizedBox(height: 30),
+                const CustomIputField( labelText: "Correo", hintText: "Correo del usuario", keyboardType: TextInputType.emailAddress,),
+                const SizedBox(height: 30),
+                const CustomIputField( labelText: "Contrasena", hintText: "Contrasena del usuario", obscureText: true),
+                const SizedBox(height: 30),
+
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text("Guardar")),
+                  ),
+                  onPressed: (){
+                    // Para desactivar el "Focus" el
+                    FocusScope.of(context).requestFocus(FocusNode());
+
+                    // Checamos la validacion de cada input
+                    if(!myFormKey.currentState!.validate()){
+                      print("formulario no valido");
+                    }
+                    print(formValues);
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       )
