88adc4d9264bce64aac78551e7e9bb2d4f7607b1
Diego-Luna <diegofcolunalopez@gmail.com>
2022-05-10 11:53:48 -0500

    Checkbox y Switches

--- fl_components_app/lib/screens/slider_screen.dart
+++ fl_components_app/lib/screens/slider_screen.dart
@@ -11,6 +11,7 @@ class SliderScreen extends StatefulWidget {
 class _SliderScreenState extends State<SliderScreen> {
 
   double _sliderValue = 100;
+  bool _sliderEnable = true;
 
   @override
   Widget build(BuildContext context) {
@@ -18,30 +19,71 @@ class _SliderScreenState extends State<SliderScreen> {
       appBar: AppBar(
         title: const Text("Slider and Checks"),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 0,
-              max: 400,
-              activeColor: AppTheme.prymary,
-              // divisions: 8,
-              value: _sliderValue,
-              onChanged: (value) {
-                print(value);
-                setState(() {
-                  _sliderValue = value;
-                });
-              },
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 400,
+            activeColor: AppTheme.prymary,
+            // divisions: 8,
+            value: _sliderValue,
+            onChanged:_sliderEnable?
+            (value) {
+              print(value);
+              setState(() {
+                _sliderValue = value;
+              });
+            }
+            :
+            null,
+          ),
+
+          Checkbox(
+            value: _sliderEnable,
+            onChanged: (value){
+              setState(() {
+                _sliderEnable = value ?? true;
+              });
+            }
+          ),
+
+          CheckboxListTile(
+            activeColor: AppTheme.prymary,
+            title: const Text("Habilitar Slider"),
+            value: _sliderEnable,
+            onChanged: (value) => setState(() {
+              _sliderEnable = value ?? true;
+            }),
+          ),
+
+          Switch(
+            value: _sliderEnable,
+            onChanged: (value) => setState(() {
+              _sliderEnable = value;
+            }),
+          ),
+
+          SwitchListTile.adaptive(
+            activeColor: AppTheme.prymary,
+            title: const Text("Habilitar Slider"),
+            value: _sliderEnable,
+            onChanged: (value) => setState(() {
+              _sliderEnable = value;
+            }),
+          ),
+
+          const AboutListTile(),
+
+          Expanded(
+            child: SingleChildScrollView(
+              child: Image(
+                image: const NetworkImage('https://www.talent-network.org/comunidades/wp-content/uploads/2019/10/MoonMakers-1.png'),
+                fit: BoxFit.contain,
+                width: _sliderValue,
+              ),
             ),
-            Image(
-              image: const NetworkImage('https://www.talent-network.org/comunidades/wp-content/uploads/2019/10/MoonMakers-1.png'),
-              fit: BoxFit.contain,
-              width: _sliderValue,
-            ),
-            const SizedBox(height: 100),
-          ],
-        ),
+          ),
+        ],
       )
     );
   }
