7cf403bdbacc7f677dc123d49704330a2904683f
Diego-Luna <diegofcolunalopez@gmail.com>
2022-07-15 20:41:09 -0500

    RefreshIndicator

--- fl_components_app/lib/screens/listview_builder_screen.dart
+++ fl_components_app/lib/screens/listview_builder_screen.dart
@@ -54,6 +54,14 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     setState(() {});
   }
 
+  Future<void> onRefresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+    add5();
+  }
+
   @override
   Widget  build(BuildContext context){
 
@@ -67,19 +75,23 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
         removeBottom: true,
         child: Stack(
           children: [
-            ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              controller: scrollController,
-              itemCount: imagesIds.length,
-              itemBuilder: (BuildContext context, int index){
-                return FadeInImage(
-                  width: double.infinity,
-                  height: 300,
-                  fit: BoxFit.cover,
-                  placeholder: const AssetImage('assets/gifs/jar-loading.gif'),
-                  image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}')
-                );
-              }
+            RefreshIndicator(
+              color: AppTheme.prymary,
+              onRefresh: onRefresh,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: scrollController,
+                itemCount: imagesIds.length,
+                itemBuilder: (BuildContext context, int index){
+                  return FadeInImage(
+                    width: double.infinity,
+                    height: 300,
+                    fit: BoxFit.cover,
+                    placeholder: const AssetImage('assets/gifs/jar-loading.gif'),
+                    image: NetworkImage('https://picsum.photos/500/300?image=${imagesIds[index]}')
+                  );
+                }
+              ),
             ),
             // nos permite ponerlo en la posicion que queramos, mientras uses Stack
             if (isLoading)
