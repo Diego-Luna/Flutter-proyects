5f9ccaaf6b0e7a38311ed2c2a955cef7abda7b0d
Diego-Luna <diegofcolunalopez@gmail.com>
2022-07-16 17:24:12 -0500

    Finalizar el MovieSlider

--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -7,15 +7,17 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 260,
+      color: Colors.transparent,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          const SizedBox(height: 10),
           const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: Text("Populares", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
           ),
+          const SizedBox(height: 10),
           Expanded(
             child: ListView.builder(
               itemCount: 20,
@@ -36,8 +38,31 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      color: Colors.transparent,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, "details", arguments: "movie-instance"),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage("assets/no-image.jpg"),
+                image: NetworkImage("https://via.placeholder.com/300x400"),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text("starWars",
+            maxLines: 2,
+            overflow:  TextOverflow.ellipsis,
+            textAlign: TextAlign.center
+          ),
+        ],
+      ),
     );
   }
 }
