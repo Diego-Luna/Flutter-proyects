90e19bf790064319d760a04f1c0d8a203866a0b2
Diego-Luna <diegofcolunalopez@gmail.com>
2022-07-17 10:55:54 -0500

    MovieDetails - Slivers - SliverAppBar

--- peliculas/lib/screens/details_screen.dart
+++ peliculas/lib/screens/details_screen.dart
@@ -5,10 +5,47 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // cambiar por una instancia
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('DetailsScreen'),
+      body: CustomScrollView(
+        // este esta echo para trabajar con slivers
+        slivers: [
+          // los tiene un conportamiento que les afecta el scroll
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.amber,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            color: Colors.black12,
+            child: const Text(
+              "Movie.title",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            )),
+        background: const FadeInImage(
+          placeholder: AssetImage("assets/loading.gif"),
+          image: NetworkImage("https://via.placeholder.com/500x300"),
+          fit: BoxFit.cover,
         ),
       ),
     );
