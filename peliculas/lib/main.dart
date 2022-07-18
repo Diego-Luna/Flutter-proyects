e7f6d06cf476879122fdb14e25fd4b045bdba3f3
Diego-Luna <diegofcolunalopez@gmail.com>
2022-07-18 08:34:17 -0500

    Provider y paquete HTTP

--- peliculas/lib/main.dart
+++ peliculas/lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
 import 'package:peliculas/screens/screens.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
+
+class AppState extends StatelessWidget {
+  // const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // nos permite tenes multiples providers(estados)
+    return MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => MoviesProvider())],
+        child: MyApp(),);
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,10 +29,9 @@ class MyApp extends StatelessWidget {
         'details': (_) => const DetailsScreen()
       },
       theme: ThemeData.light().copyWith(
-        appBarTheme: const AppBarTheme(
-          color: Colors.orange,
-        )
-      ),
+          appBarTheme: const AppBarTheme(
+        color: Colors.orange,
+      )),
     );
   }
 }
