a7e1f27a7f3adc0b36d717b52badb8874c4a9349
Diego-Luna <diegofcolunalopez@gmail.com>
2022-07-19 07:57:48 -0500

    Peticiones HTTP

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,13 +1,27 @@
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class MoviesProvider extends ChangeNotifier{
-  MoviesProvider(){
+class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '2a8fb32f0377e4544ced157b55a716b4';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
+
+  MoviesProvider() {
     print("MoviesProvider inicialisado");
     this.getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() async{
-    print("getOnDisplayMovies");
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': "1"});
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(decodedData);
   }
 }
