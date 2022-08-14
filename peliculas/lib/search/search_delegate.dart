7dd83cfa12c746c3a12434b727ffdd923c1dddd6
Diego-Luna <diegofcolunalopez@gmail.com>
2022-08-13 20:53:25 -0500

    Mostrar las películas basado en el query

--- peliculas/lib/search/search_delegate.dart
+++ peliculas/lib/search/search_delegate.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
+
+import '../models/movie.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -32,18 +36,62 @@ class MovieSearchDelegate extends SearchDelegate {
     return Text("buildResults");
   }
 
+  Widget _EmptyContainer() {
+    return Container(
+        child: const Center(
+      child:
+          Icon(Icons.movie_creation_outlined, color: Colors.black38, size: 100),
+    ));
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
     // TODO: implement buildSuggestions
     // throw UnimplementedError();
-    if(query.isEmpty){
-      return Container(
-        child:  const Center(
-          child: Icon(Icons.movie_creation_outlined, color: Colors.black38, size: 100),
-        ),
-      );
+    if (query.isEmpty) {
+      return _EmptyContainer();
+      ;
     }
 
-    return Container();
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+        future: moviesProvider.searchMovies(query),
+        builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+          if (!snapshot.hasData) {
+            return _EmptyContainer();
+          }
+
+          final movies = snapshot.data!;
+
+          return ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (_, int index) => _MovieItem(movies[index]));
+        });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    movie.heroId = 'search-${movie.id}';
+
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage("assets/no-image.jpg"),
+        image: NetworkImage(movie.fullPosterImg),
+        width: 50,
+        fit: BoxFit.contain,
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: (){
+        Navigator.pushNamed(context, "details", arguments: movie);
+      },
+    );
   }
 }
