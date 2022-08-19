512d362101efa1177548e42a62957d087c9cfd3e
Diego-Luna <diegofcolunalopez@gmail.com>
2022-08-18 20:10:22 -0500

    Hero Animation

--- peliculas/lib/search/search_delegate.dart
+++ peliculas/lib/search/search_delegate.dart
@@ -81,15 +81,18 @@ class _MovieItem extends StatelessWidget {
     movie.heroId = 'search-${movie.id}';
 
     return ListTile(
-      leading: FadeInImage(
-        placeholder: const AssetImage("assets/no-image.jpg"),
-        image: NetworkImage(movie.fullPosterImg),
-        width: 50,
-        fit: BoxFit.contain,
+      leading: Hero(
+        tag: movie.heroId!,
+        child: FadeInImage(
+          placeholder: const AssetImage("assets/no-image.jpg"),
+          image: NetworkImage(movie.fullPosterImg),
+          width: 50,
+          fit: BoxFit.contain,
+        ),
       ),
       title: Text(movie.title),
       subtitle: Text(movie.originalTitle),
-      onTap: (){
+      onTap: () {
         Navigator.pushNamed(context, "details", arguments: movie);
       },
     );
--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -77,6 +77,8 @@ class _MoviePoster extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    movie.heroId = "slider-popular-${movie.id}";
+
     return Container(
       width: 130,
       height: 190,
@@ -85,16 +87,19 @@ class _MoviePoster extends StatelessWidget {
       child: Column(
         children: [
           GestureDetector(
-            onTap: () => Navigator.pushNamed(context, "details",
-                arguments: movie),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: const AssetImage("assets/no-image.jpg"),
-                image: NetworkImage(movie.fullPosterImg),
-                width: 130,
-                height: 190,
-                fit: BoxFit.cover,
+            onTap: () =>
+                Navigator.pushNamed(context, "details", arguments: movie),
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: const AssetImage("assets/no-image.jpg"),
+                  image: NetworkImage(movie.fullPosterImg),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
