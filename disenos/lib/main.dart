73ced7825913aee6889bc830591e361ee4167d7d
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-16 10:45:52 -0500

    Diseño con scroll - parte 1

--- disenos/lib/main.dart
+++ disenos/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:disenos/src/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
 import 'package:disenos/src/screens/basic_design.dart';
 
@@ -6,13 +7,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Material App',
-        initialRoute: 'basic_design',
-        routes: {
-          'basic_design':(_) => BasicDesesignScreen()
-        },
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'scrpll_design',
+      routes: {
+        'basic_design': (_) => BasicDesesignScreen(),
+        'scrpll_design': (_) => ScrollScreen()
+      },
     );
   }
 }
