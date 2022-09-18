bd9ed5f3d55bf308f5acb05351355ca9b49e22f2
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-18 09:17:57 -0500

    Diseño compuesto - Blur

--- disenos/lib/src/widgets/card_table.dart
+++ disenos/lib/src/widgets/card_table.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -40,27 +42,36 @@ class _SigleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-          color: const Color.fromRGBO(62, 66, 107, 0.7),
-          borderRadius: BorderRadius.circular(20)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            child: Icon(
-              icon,
-              size: 30,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            height: 180,
+            decoration: BoxDecoration(
+                color: const Color.fromRGBO(62, 66, 107, 0.7),
+                borderRadius: BorderRadius.circular(20)),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  child: Icon(
+                    icon,
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  radius: 30,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  text,
+                  style: TextStyle(color: color, fontSize: 18),
+                )
+              ],
             ),
-            radius: 30,
           ),
-          SizedBox(height: 20),
-          Text(
-            text,
-            style: TextStyle(color: color, fontSize: 18),
-          )
-        ],
+        ),
       ),
     );
   }
