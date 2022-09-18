1570615eea930cd558f4ac6a43e1fbfe0b9b7627
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-18 08:36:58 -0500

    Diseño compuesto - BottomNavigationBar 2

--- disenos/lib/src/widgets/custom_bottom_navigation.dart
+++ disenos/lib/src/widgets/custom_bottom_navigation.dart
@@ -3,13 +3,23 @@ import 'package:flutter/material.dart';
 class CustomBottomNavigation extends StatelessWidget {
   const CustomBottomNavigation({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      width: double.infinity,
-      height: 200,
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedItemColor: Colors.pink,
+      backgroundColor: const Color.fromRGBO(55, 57, 84, 1),
+      unselectedItemColor: const Color.fromRGBO(116, 116, 152, 1),
+      currentIndex: 1,
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today_outlined), label: "Calendario"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart_outline_outlined), label: "Grafica"),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle_outlined), label: "Usuarios"),
+      ],
     );
   }
 }
