b2ab743ae528c15a30f66fc7e719f99ecbd94fbf
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-19 07:52:49 -0500

    Diseño de la aplicación

--- app_qr_reader/lib/src/pages/home_page.dart
+++ app_qr_reader/lib/src/pages/home_page.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_navigatorbar.dart';
+import '../widgets/scan_button.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Historia'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.delete_forever))
+        ],
+      ),
+      body: const Center(
         child: Text('Hola Mundo'),
-     ),
-   );
+      ),
+      bottomNavigationBar: const CustomNavigatorBar(),
+      floatingActionButton: const ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
