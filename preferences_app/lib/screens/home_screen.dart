e268f1c19e561f264f4a51e3703d356a0e10f4d7
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-24 11:07:04 -0500

    Drawers - Crear un menú latera

--- preferences_app/lib/screens/home_screen.dart
+++ preferences_app/lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   static const String routerName = 'Home';
 
@@ -11,6 +13,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
+      drawer: const SideMenu(),
       body: const Center(
         child: Text('HomeScreen'),
       ),
--- preferences_app/lib/screens/settings_screen.dart
+++ preferences_app/lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class SettingsScreen extends StatelessWidget {
 
   static const String routerName = 'Settings';
@@ -11,6 +13,7 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
       ),
+      drawer: const SideMenu(),
       body: const Center(
         child: Text('SettingsScreen'),
       ),
