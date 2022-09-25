626ab45c1a3e855435179b9d454252c2cae4182a
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-25 12:46:03 -0500

    Hacer funcionar los Switches y Radios

--- preferences_app/lib/screens/settings_screen.dart
+++ preferences_app/lib/screens/settings_screen.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../widgets/widgets.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   static const String routerName = 'Settings';
   const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool isDarkMode = false;
+  int geneder = 1;
+  String name = 'Diego';
 
   @override
   Widget build(BuildContext context) {
@@ -23,31 +32,43 @@ class SettingsScreen extends StatelessWidget {
               ),
               const Divider(),
               SwitchListTile.adaptive(
-                value: true,
-                onChanged: (value) {},
+                value: isDarkMode,
+                onChanged: (value) {
+                  isDarkMode = value;
+                  setState(() {});
+                },
                 title: const Text('Dark mode'),
               ),
               const Divider(),
               RadioListTile<int>(
                   value: 1,
-                  groupValue: 1,
+                  groupValue: geneder,
                   title: const Text('Masculino'),
-                  onChanged: (value) {}),
+                  onChanged: (value) {
+                    geneder = value ?? 1;
+                    setState(() {});
+                  }),
               const Divider(),
               RadioListTile<int>(
                   value: 2,
-                  groupValue: 1,
+                  groupValue: geneder,
                   title: const Text('Femenino'),
-                  onChanged: (value) {}),
+                  onChanged: (value) {
+                    geneder = value ?? 2;
+                    setState(() {});
+                  }),
               const Divider(),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: TextFormField(
-                  initialValue: 'Fernando',
+                  initialValue: name,
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
                   decoration: const InputDecoration(
-                      labelText: 'Nombre',
-                      helperText: 'Nombre del usuario'
-                  ),
+                      labelText: 'Nombre', helperText: 'Nombre del usuario'),
                 ),
               )
             ],
