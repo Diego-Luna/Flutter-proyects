931a3d5ec3c58bdb8973eb1b0b7c88efe306dcf2
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-26 09:47:09 -0500

    Formulario de Login

--- productos_app/lib/src/screens/login_screen.dart
+++ productos_app/lib/src/screens/login_screen.dart
@@ -20,12 +20,48 @@ class LoginScreen extends StatelessWidget {
                   style: Theme.of(context).textTheme.headline4,
                 ),
                 SizedBox(height: 30),
-                Text('Formulario')
+                _LoginForm()
               ]),
             ),
+            SizedBox(height: 50),
+            Text(
+              'Crear una nueva cuenta',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            )
           ],
         ),
       ),
     ));
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // todo> mantener la referencia al key
+      child: Form(
+          child: Column(
+        children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.deepPurple),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
+                hintText: 'john.hello@gmail.com',
+                labelText: 'Corre electronico',
+                labelStyle: TextStyle(color: Colors.grey),
+                prefixIcon: Icon(
+                  Icons.alternate_email_sharp,
+                  color: Colors.deepPurple,
+                )),
+          )
+        ],
+      )),
+    );
   }
 }
