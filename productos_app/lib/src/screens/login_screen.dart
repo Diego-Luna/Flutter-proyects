9b02a19003dc531241a4136f2f66716ceb230c30
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-27 08:27:42 -0500

    Validaciones del formulario

--- productos_app/lib/src/screens/login_screen.dart
+++ productos_app/lib/src/screens/login_screen.dart
@@ -10,26 +10,26 @@ class LoginScreen extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 250,
             ),
             CardContainer(
               child: Column(children: [
-                SizedBox(height: 10),
+                const SizedBox(height: 10),
                 Text(
                   'Login',
                   style: Theme.of(context).textTheme.headline4,
                 ),
-                SizedBox(height: 30),
+                const SizedBox(height: 30),
                 _LoginForm()
               ]),
             ),
-            SizedBox(height: 50),
-            Text(
+            const SizedBox(height: 50),
+            const Text(
               'Crear una nueva cuenta',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
           ],
         ),
       ),
@@ -43,50 +43,65 @@ class _LoginForm extends StatelessWidget {
     return Container(
       // todo> mantener la referencia al key
       child: Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: 'john.dow@gmail.com',
-                labelText: 'Correo electronico',
-                prefixIcon: Icons.alternate_email_rounded),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            autocorrect: false,
-            obscureText: true,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: '****',
-                labelText: 'Password',
-                prefixIcon: Icons.lock_outline),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          MaterialButton(
-            onPressed: () {
-              // Todo: Login form
-            },
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            disabledColor: Colors.grey,
-            elevation: 0,
-            color: Colors.deepPurple,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-              child: Text(
-                'Ingresar',
-                style: TextStyle(color: Colors.white),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'john.dow@gmail.com',
+                    labelText: 'Correo electronico',
+                    prefixIcon: Icons.alternate_email_rounded),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El correo no es correcto';
+                },
               ),
-            ),
-          )
-        ],
-      )),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '****',
+                    labelText: 'Password',
+                    prefixIcon: Icons.lock_outline),
+                validator: (value) {
+                  if (value != null && value.length >= 6) return null;
+
+                  return 'La contrasena de ve de ser de 6 caracteres';
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  // Todo: Login form
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                  child: const Text(
+                    'Ingresar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
