cf2e731edbbfc167f8da665603fbc1aca51888a4
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-28 12:21:28 -0500

    Modelo y Servicio de Productos

--- productos_app/lib/main.dart
+++ productos_app/lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/src/services/services.dart';
+import 'package:provider/provider.dart';
 
 import 'src/screens/screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductsService())
+      ],
+      child: MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,9 +38,7 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.grey[300],
           appBarTheme: AppBarTheme(elevation: 0, color: Colors.orange),
           floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: Colors.orange,
-            elevation: 0
-          )),
+              backgroundColor: Colors.orange, elevation: 0)),
     );
   }
 }
---
+++ productos_app/lib/src/services/products_service.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/src/models/product.dart';
+import 'package:provider/provider.dart';
+
+class ProductsService extends ChangeNotifier {
+  final String _baseUrl = '';
+
+  final List<Product> products = [];
+
+  // hacer fetch
+}
