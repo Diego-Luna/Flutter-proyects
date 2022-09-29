c51e0113a59b0006c931c75aad26b5bc4ad414e3
Diego-Luna <diegofcolunalopez@gmail.com>
2022-09-29 09:39:42 -0500

    Cargar los productos desde Firebase

--- productos_app/lib/src/screens/home_screen.dart
+++ productos_app/lib/src/screens/home_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/src/services/services.dart';
 import 'package:productos_app/src/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final productsService = Provider.of<ProductsService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Productos'),
--- productos_app/lib/src/services/products_service.dart
+++ productos_app/lib/src/services/products_service.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:productos_app/src/models/product.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
 
 class ProductsService extends ChangeNotifier {
-  final String _baseUrl = '';
-
+  final String _baseUrl =
+      '';
   final List<Product> products = [];
+  bool isLoading = true;
 
-  // hacer fetch
+  ProductsService() {
+    this.loadProducts();
+  }
+
+  // Future<List<Product>> loadProducts() async {
+  Future loadProducts() async {
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+
+    productsMap.forEach((key, value) {
+      final tempProduct = Product.fromMap(value);
+      tempProduct.id = key;
+      this.products.add(tempProduct);
+    });
+  }
 }
