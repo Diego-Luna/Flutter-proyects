60d700e942fc01f760b944241cb9d9386502bb5f
Diego-Luna <diegofcolunalopez@gmail.com>
2022-10-06 08:42:28 -0500

    Usar token para validar peticiones en Firebase

--- productos_app/lib/main.dart
+++ productos_app/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Productos App',
-      initialRoute: 'checking',
+      initialRoute: 'login',
       routes: {
         'login': (_) => LoginScreen(),
         'register': (_) => const RegisterScreen(),
--- productos_app/lib/src/services/products_service.dart
+++ productos_app/lib/src/services/products_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:productos_app/src/models/product.dart';
 import 'package:http/http.dart' as http;
 
@@ -14,6 +15,8 @@ class ProductsService extends ChangeNotifier {
   bool isLoading = true;
   bool isSaving = false;
 
+  final storage = new FlutterSecureStorage();
+
   ProductsService() {
     this.loadProducts();
   }
@@ -22,7 +25,9 @@ class ProductsService extends ChangeNotifier {
     this.isLoading = true;
     notifyListeners();
 
-    final url = Uri.https(_baseUrl, 'products.json');
+    final url = Uri.https(_baseUrl, 'products.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
     final resp = await http.get(url);
 
     final Map<String, dynamic> productsMap = json.decode(resp.body);
@@ -55,7 +60,9 @@ class ProductsService extends ChangeNotifier {
   }
 
   Future<String> createProduct(Product product) async {
-    final url = Uri.https(_baseUrl, 'products.json');
+    final url = Uri.https(_baseUrl, 'products.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
     final resp = await http.post(url, body: product.toJson());
 
     final decodedData = json.decode(resp.body);
@@ -68,7 +75,9 @@ class ProductsService extends ChangeNotifier {
   }
 
   Future<String> updateProduct(Product product) async {
-    final url = Uri.https(_baseUrl, 'products/${product.id}.json');
+    final url = Uri.https(_baseUrl, 'products/${product.id}.json', {
+      'auth': await storage.read(key: 'token') ?? ''
+    });
     final resp = await http.put(url, body: product.toJson());
 
     final decodedData = resp.body;
